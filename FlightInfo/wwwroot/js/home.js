81717d120d80278057023ea77259616f33c501c2
Omri Tcherner <omimako@gmail.com>
2021-10-08 18:11:17 +0300

    Modified landing page - twitter, video and js libraries

---
+++ FlightInfo/wwwroot/js/home.js
@@ -0,0 +1,23 @@
+﻿$(function () {
+    $.ajax({
+        method: "GET",
+        url: "/Hospitals/PatientsByHospitalCount",
+        success: function (data) {
+            alert("success");
+        },
+        error: function (data) {
+            alert("error!");
+        }
+    });
+
+    $.ajax({
+        method: "GET",
+        url: "Doctors/PatientsForDoctorStatistic",
+        success: function (data) {
+            alert("success");
+        },
+        error: function (data) {
+            alert("error!");
+        }
+    });
+});
