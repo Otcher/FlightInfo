ff1c52f35767e19eb3fef810d063dc834790e6c5
Oz <o23456789@gmail.com>
2021-10-12 17:25:40 +0300

    lol

--- FlightInfo/Controllers/CountriesController.cs
+++ FlightInfo/Controllers/CountriesController.cs
@@ -195,13 +195,13 @@ namespace FlightInfo.Controllers
         // GET: Countries/FlightsToCountries
         public IActionResult FlightsToCountries()
         {
-            var AirpotsInCountries = from f in _context.Flight
+            var FlightsToCountries = from f in _context.Flight
                                      join c in _context.City
                                      on f.Destination.CityId equals c.Id
                                      group f by c.Country.Name into g
                                      select new { CountryName = g.Key, FlightsCount = g.Count() };
 
-            return Json(AirpotsInCountries.ToList());
+            return Json(FlightsToCountries.ToList());
         }
     }
 }
